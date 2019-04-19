7299ba16fc276c5de414b2152d2addd49ec22881
zergweb <zergwebprot@gmail.com>
2019-04-19 19:23:06 +0300

    init

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="home">
+      <el-button type="primary" plain>Primary</el-button>
+      sdfs
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
