7719ca065a90c816101f7bfdc3def9ddc3547922
zergweb <zergwebprot@gmail.com>
2019-05-14 14:08:09 +0300

    add users list with pagination

--- src/Pages/Admin/UsersListPage.vue
+++ src/Pages/Admin/UsersListPage.vue
@@ -1,8 +1,40 @@
 <template>
     <div>
         <PageBlueHeader name="Userslist" />
-        <el-main>
-            <h1>UsersListPage</h1>
+        <el-main class="list">
+            <el-table ref="filterTable"
+                      :data="currentUsers"
+                      style="width: 80%">
+                <el-table-column label="FIO">
+                    <template slot-scope="scope">
+                        <span>{{ scope.row.lName }} </span>
+                        <span>{{ scope.row.fName }} </span>
+                    </template>
+                </el-table-column>
+
+                <el-table-column prop="balance"
+                                 label="Balance(Rub)"
+                                 sortable>
+                </el-table-column>
+                <el-table-column prop="license"
+                                 label="License(Days)"
+                                 sortable>
+                </el-table-column>
+                <el-table-column label="Operations">
+                    <template slot-scope="scope">
+                        <el-button size="mini">Edit</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+
+            <el-pagination :page-size="itemsPerPage"
+                           style="margin-top:30px;"
+                           layout="prev, pager, next"
+                           :total="users.length"
+                            @size-change="handleSizeChange"
+                            @current-change="handleCurrentChange"
+                           >
+            </el-pagination>
         </el-main>
     </div>
 </template>
@@ -13,6 +45,41 @@ export default {
   name: 'home',
   components: {
       PageBlueHeader
-        }      
+        },
+  data() {
+            return {
+                itemsPerPage: 10,
+                currentPage: 1,
+                
+            };
+        },
+  methods: {
+            handleSizeChange(val) {
+                console.log(`${val} items per page`);
+            },
+            handleCurrentChange(val) {
+                this.currentPage = val;
+            }
+       },
+  computed: {
+            currentUsers: function () {
+                return this.$store.getters.allUsers.slice((this.currentPage-1) * this.itemsPerPage,this.currentPage*this.itemsPerPage);
+            },
+            users: function () {
+                return this.$store.getters.allUsers;
+            }
+        },
+  created: function () {
+      this.$store.dispatch('loadUsers');
+        }
 }
 </script>
+<style>
+    .list {
+        align-items: center;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        background-color: #fff;
+    }
+</style>
